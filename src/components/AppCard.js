683f70866191750c6e175330ba8d8a0ab0398435
Praba Ponnambalam <praba@codechilli.lk>
2024-09-21 03:01:02 +0530

    💄 adjust image size on app cards

--- src/components/AppCard.js
+++ src/components/AppCard.js
@@ -4,7 +4,9 @@ export function AppCard(app) {
 	const card = document.createElement('div');
 	card.className = 'card h-100 clickable-card';
 	card.innerHTML = `
-    <img src="${app.logo}" class="card-img-top" alt="${app.name} logo">
+    <div class="card-img-top-wrapper">
+      <img src="${app.logo}" class="card-img-top img-fluid" alt="${app.name} logo">
+    </div>
     <div class="card-body d-flex flex-column">
       <h5 class="card-title">${app.name}</h5>
       <p class="card-text flex-grow-1">${app.description}</p>
