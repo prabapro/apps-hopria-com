dbef8cfbb1e2dc1662f7ab93f26138a2d19f2678
Praba Ponnambalam <praba@codechilli.lk>
2024-09-21 03:37:53 +0530

    💄 add padding to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ export function Footer() {
 	return `
         <div class="container py-3">
             <div class="row">
-                <div class="col-md-8 offset-md-2 border-top pt-4">
+                <div class="col-md-8 offset-md-2 border-top pt-4 pb-2 px-4">
                     <div class="d-flex justify-content-between align-items-center">
                         <small>&copy; ${currentYear} - All rights reserved.</small>
-                        <small><a href="/sitemap" class="text-muted footer-nav-link" data-nav-item="sitemap">Sitemap</a></small>
+                        <small><a href="/sitemap" class="footer-nav-link" data-nav-item="sitemap">Sitemap</a></small>
                     </div>
                 </div>
             </div>
